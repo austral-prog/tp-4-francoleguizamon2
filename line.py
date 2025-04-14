0d2e7eb4e0ca0850883ce932686b9b038dd3fbaa
Franco <ftleguizamon@mail.austral.edu.ar>
2025-04-14 11:14:54 -0300

    Update line.py

--- line.py
+++ line.py
@@ -1,2 +1,31 @@
+import math
+
+def ecuacion(m, b, x):
+    resultado = m * x + b
+    return resultado
+
 def line():
-    print("TO DO")
+    a = 2.3
+    b = -4
+    x1 = 50
+    x2 = -32.9
+    print('Ingrese el coeficiente A:', a)
+    print('Ingrese el coeficiente B:', b)
+    print('Ingrese el coeficiente X1:', x1)
+    print('Ingrese el coeficiente X2:', x2)
+    print('El coeficiente A de su ecuación de la recta es:', a)
+    print('El coeficiente B de su ecuación de la recta es:', b)
+    print('El coeficiente X1 de su ecuación de la recta es:', x1)
+    print('El coeficiente X2 de su ecuación de la recta es:', x2)
+    print('')
+    print('Para la siguiente ecuación:')
+    print('\tY =', a, 'X +', b)
+    print('')
+    print('Dados los siguientes puntos:')
+    punto1 = x1, ecuacion(2.3, -4, x1)
+    print('\tP1', (punto1))
+    punto2 = x2, ecuacion(2.3, -4, x2)
+    print('\tP2', (punto2))
+    print('')
+    print('La distancia entre ellos es: ', math.dist(punto1, punto2))
+line()
